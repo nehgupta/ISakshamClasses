1c15ce12fd64268d3b306542c0aa86b247cdce0a
Neha Gupta <nehgupta@amazon.com>
2015-12-24 22:03:22 +0530

    https://github.com/nehgupta/ISakshamClasses/issues/1
    Initial Commit.

---
+++ src/com/isaksham/course/activities/ClassesActivity.java
@@ -0,0 +1,15 @@
+package com.isaksham.course.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.isankalan.R;
+
+public class ClassesActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.classes_activity);
+	}
+
+}
